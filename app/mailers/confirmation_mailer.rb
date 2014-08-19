6a5d245fb936cb2ab173af4b1f01acd221cf933a
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-08-19 16:59:55 -0400

    Edit content on mailer

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -2,6 +2,6 @@ class ConfirmationMailer < ActionMailer::Base
   default from: "confirmations@shipyardrehearsals.com"
 
   def confirmation_email(email)
-    mail(to: email, subject: "Confirming your sessions at Shipyard Rehearsals")
+    mail(to: email, subject: "Confirming your session(s) at Shipyard Rehearsals")
   end
 end
