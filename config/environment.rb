cb7119cfe3967d31b612cfd22d99df335d59cf79
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-08-20 10:47:52 -0400

    Add mail configurations to environment.rb:

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,16 @@
-# Load the Rails application.
 require File.expand_path('../application', __FILE__)
+require 'mail'
 
-# Initialize the Rails application.
+Mail.defaults do
+  delivery_method :smtp, {
+    :address => 'smtp.sendgrid.net',
+    :port => '587',
+    :domain => 'heroku.com',
+    :user_name => ENV['SENDGRID_USERNAME'],
+    :password => ENV['SENDGRID_PASSWORD'],
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+end
+
 Rails.application.initialize!
