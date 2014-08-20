6122caf6a82e20cd42cf0721d6ddfd6a5d477974
Phillip Gutowski <phillip.gutowski@gmail.com>
2014-08-20 10:50:40 -0400

    Change from email address to livelock@heroku.com

--- app/mailers/confirmation_mailer.rb
+++ app/mailers/confirmation_mailer.rb
@@ -1,5 +1,5 @@
 class ConfirmationMailer < ActionMailer::Base
-  default from: "confirmations@shipyardrehearsals.com"
+  default from: "livelock@heroku.com"
 
   def confirmation_email(email)
     mail(to: email, subject: "Confirming your session(s) at Shipyard Rehearsals")
